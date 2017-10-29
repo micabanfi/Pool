9fcc3827d35e5094825a1933c7b9879adc1cdd94
MicaBanfi <micabanfi@hotmail.com>
2017-10-29 19:34:43 -0300

    Agrego birthDate

--- src/root/User/Person.java
+++ src/root/User/Person.java
@@ -8,15 +8,17 @@ public class Person {
     private String phone;
     private Rating rating;
     private String gender;
+    private Date birthDate;
     private Preferences preferences;
     private ArrayList<Vehicle> vehicles;
 
-    public Person(String name, String surname, String career, String phone,boolean smoke,boolean food,String gender){
+    public Person(String name, String surname, String career, String phone,boolean smoke,boolean food,String gender,Date birthDate){
         this.name=name;
         this.surname=surname;
         this.phone=phone;
         this.preferences = new Preferences(career, smoke, food);
         this.gender=gender;
+        this.birthDate=birthDate;
         //inicializar raiting
     }
 
